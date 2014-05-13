73f1a63c0999968230ea565ea45090aa766590ea
fooeyandnuts <william.sipes@gmail.com>
2014-05-13 14:57:06 -0700

    rails new for Trader Joes app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TraderJoes::Application.config.secret_key_base = '518a57a37e4f02891fc6e8ca43a60562ace5048e78163aeb96894bf20770f2069235075c8feb2b56ed1d5c6201a8464cae82d91d2b03720ccf59cda92362b961'
